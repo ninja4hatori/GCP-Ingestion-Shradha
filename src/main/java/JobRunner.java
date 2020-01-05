2bd8fcbfe47753d02856759ff7881a675ce4edb8
ninja4hatori <59428837+ninja4hatori@users.noreply.github.com>
2020-01-06 03:35:12 +0530

    Update JobRunner.java

--- src/main/java/JobRunner.java
+++ src/main/java/JobRunner.java
@@ -4,7 +4,7 @@ public class JobRunner {
     public static void main(String[] args) {
         try {
             long outputRows = LoadData.load();
-            System.out.println(outputRows);
+            System.out.println("Number of records successfully loaded: "+outputRows);
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (IOException e) {
