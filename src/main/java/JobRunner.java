f65a5ebeba403d76ba5e9ed581407a592c64b2e7
ninja4hatori <59428837+ninja4hatori@users.noreply.github.com>
2020-01-07 03:32:36 +0530

    Java Code to load Avro files to BigQuery GCP

--- src/main/java/JobRunner.java
+++ src/main/java/JobRunner.java
@@ -4,7 +4,10 @@ public class JobRunner {
     public static void main(String[] args) {
         try {
             long outputRows = LoadData.load();
-            System.out.println(outputRows);
+            System.out.println("Number of records successfully loaded: " + outputRows);
+            System.out.println("Querying the data now.");
+            LoadData.query();
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (IOException e) {
--- src/main/java/LoadData.java
+++ src/main/java/LoadData.java
@@ -2,24 +2,25 @@
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.bigquery.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.channels.Channels;
+import java.util.ArrayList;
 
 import com.google.common.io.Files;
 
-public class LoadData {
-public static Long load() throws InterruptedException, IOException {
-    String jsonPath = "C:\\Users\\Rahul\\Desktop\\GCP\\Key\\My First Project-5bea8475ed1c.json";
-    GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(jsonPath));
+import org.apache.spark.sql.*;
 
+public class LoadData {
+    public static String jsonPath = "C:\\Users\\Rahul\\Desktop\\GCP\\Key\\My First Project-5bea8475ed1c.json";
+    public static String datasetName = "SampleAvro";
+    public static String tableName="Sample";
+    public static String location = "asia-south1";
+    public static String fPath = "C:\\Users\\Rahul\\IdeaProjects\\GCP-Ingestion\\src\\main\\resources\\SampleFiles\\userdata1.avro";
+
+    public static Long load() throws InterruptedException, IOException {
+    GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(jsonPath));
     BigQuery bigquery = BigQueryOptions.newBuilder().setCredentials(credentials).build().getService();
-    String datasetName = "SampleAvro";
-    String tableName="Sample";
-    String location = "asia-south1";
-    File filePath = new File("C:\\Users\\Rahul\\IdeaProjects\\GCP-Ingestion\\src\\main\\resources\\SampleFiles\\userdata1.avro");
+    File filePath = new File(fPath);
     TableId tableId = TableId.of(datasetName, tableName);
     WriteChannelConfiguration writeChannelConfiguration =
             WriteChannelConfiguration.newBuilder(tableId).setFormatOptions(FormatOptions.avro()).build();
@@ -38,4 +39,35 @@ public static Long load() throws InterruptedException, IOException {
     return stats.getOutputRows();
 
 }
+
+public static void query () throws IOException, InterruptedException {
+    GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(jsonPath));
+
+    BigQuery bigquery = BigQueryOptions.newBuilder().setCredentials(credentials).build().getService();
+    String query = "SELECT id, first_name, last_name FROM `" + datasetName + "." + tableName +"` where country = \"Indonesia\";";
+    QueryJobConfiguration queryConfig = QueryJobConfiguration.newBuilder(query).build();
+    //DataFrame df;
+// Print the results.
+    for (FieldValueList row : bigquery.query(queryConfig).iterateAll()) {
+        //df = row.toArray();
+        int colNumber=0;
+        for (FieldValue val : row) {
+           // System.out.printf("%s,", val.toString());
+            switch (colNumber){
+                case 0:
+                    System.out.println("id:" + val.getStringValue());
+                    break;
+                case 1:
+                    System.out.println("first_name:" + val.getStringValue());
+                    break;
+                case 2:
+                    System.out.println("last_name:" + val.getStringValue());
+                    break;
+            }
+            colNumber++;
+        }
+        System.out.printf("\n");
+    }
 }
+}
+
